9c75bcc0b559de102127506d4adf4838099f9a47
masoud harooni <masoudharooni50@gmail.com>
2022-10-02 19:01:33 +0330

    Done activity area and its actions

---
+++ js/additional_registration/activity_area_modal.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+
+    $('#activity-area-plus-icon').click(function (e) {
+        $('#activity-area-modal').css('display', 'block');
+    });
+
+    $('#activity-area-cansell-btn').click(function (e) {
+
+        $('#activity-area-modal').css('display', 'none');
+    });
+
+    $('#activity-area-save-icon').click(function (e) {
+        alert("سیو شد");
+    });
+
+    $('form#activity-area-form').submit(function (e) {
+        e.preventDefault();
+        let countOfRows = document.getElementById('activity-area-table').rows.length;
+        let currentRow = countOfRows++;
+        const form = $('form#activity-area-form');
+        let formData = form.serializeArray();
+        $(`
+            <tr>
+                <td data-rowId="${currentRow}"></td>
+                <td>${formData[0]['value']}</td>
+                <td>${formData[1]['value']}</td>
+                <td>${formData[2]['value']}</td>
+                <td class="delete-icon-container" title="پاک کردن">
+                    <i data-deleteId="${currentRow}" class="fas fa-trash delete-icon  delete-activity-area-icon"></i>
+                </td>
+            </tr>
+        `).appendTo('table#activity-area-table');
+        $('#activity-area-modal').css('display', 'none');
+        clearFormInputs(form);
+        // English numbers to Persian
+        traverse(document.body);
+
+
+        // remove address
+        $('.delete-activity-area-icon').click(function (e) {
+            const id = $(this).attr('data-deleteId');
+            deleteRow('activity-area-table', id);
+        });
+        function deleteRow(tableId, expectedId) {
+            let tableRows = document.getElementById(tableId).rows;
+            for (let index = 1; index < tableRows.length; index++) {
+                let row = tableRows[index];
+                let rowId = row.querySelector('td').getAttribute('data-rowId');
+                if (rowId == expectedId) {
+                    row.remove();
+                }
+            }
+        }
+
+        function clearFormInputs(form) {
+            form.find("input[type=text], input[type=number],input[type=email]").val("");
+        }
+    });
+
+    // convertion english numbers to persian
+    persian = { 0: '۰', 1: '۱', 2: '۲', 3: '۳', 4: '۴', 5: '۵', 6: '۶', 7: '۷', 8: '۸', 9: '۹' };
+    function traverse(el) {
+        if (el.nodeType == 3) {
+            var list = el.data.match(/[0-9]/g);
+            if (list != null && list.length != 0) {
+                for (var i = 0; i < list.length; i++)
+                    el.data = el.data.replace(list[i], persian[list[i]]);
+            }
+        }
+        for (var i = 0; i < el.childNodes.length; i++) {
+            traverse(el.childNodes[i]);
+        }
+    }
+    // convertion english numbers to persian
+
+});
